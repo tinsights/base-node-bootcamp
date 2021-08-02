745ddca24a8e98b965079dfb04b75a9bc566ed06
Tinaes <aj.tinaes@gmail.com>
2021-08-02 18:46:10 +0800

    exercises done

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-import { get } from 'http';
+// createServer comes from the http module built-in to Node.
+import { createServer, get } from 'http';
+
+const handleIncomingRequest = (request, response) => {
+  console.log('Received request!');
+  response.writeHead(200, {});
+  // response.end tells the server to send the completed response and mark
+  // this request-response interaction complete.
+  // https://nodejs.org/api/http.html#http_response_end_data_encoding_callback
+  response.end('Yay!', 'utf-8');
+};
+
+// createServer creates the server object. It accepts a request listener function.
+// The server calls the function every time it receives a request.
+// The listen method tells server to start listening for requests on given port.
+
+createServer(handleIncomingRequest).listen(3004);
 
 const handleResponse = (response) => {
   // Compile response data in a data variable.
@@ -15,9 +31,3 @@ const handleResponse = (response) => {
     console.log('Response Data: ', data);
   });
 };
-
-// Send an HTTP GET request, handle response with handleResponse callback.
-// Handle errors by logging the error message.
-get('http://info.cern.ch/', handleResponse).on('error', (err) => {
-  console.error(`Error: ${err.message}`);
-});
