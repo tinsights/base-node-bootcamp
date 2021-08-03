6ed62aa8d2b000ab2c3715b8aaf3269ba7d38519
Tinaes <aj.tinaes@gmail.com>
2021-08-03 10:50:29 +0800

    base

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+import { write, read, add } from './jsonFileStorage.js';
+
+// Get some data from our app
+const myData = { name: 'kai' };
+// Save that data to our JSON DB
+add('data.json', 'name', 'kai');
